0ef43526fe637d4c4f6f045d7c9cad6c123e43ed
Visoth YOUK-SIM <115548934+visothyouksim@users.noreply.github.com>
2024-08-28 15:09:29 +0200

    Étape 1 : Mise en place de l'environnement de test -- OK

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+        
+        $this->assertEquals(2, 1 + 1);
+    }
+}
